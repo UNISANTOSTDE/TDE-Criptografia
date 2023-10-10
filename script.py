768f544f3f63cb3b895f884867716c2027a0728e
João Falcão <jgabfalcao@gmail.com>
2023-10-10 20:25:10 -0300

    Inital commit

--- script.py
+++ script.py
@@ -1,25 +1,51 @@
-def criptografia(palavra):
-    mensagem = " "
+# def criptografia(palavra):
+#     mensagem = " "
 
-    for i in palavra:
-        mensagem = mensagem + chr(ord(i) + 5)
-    return mensagem
-
-
-def descriptografar(mensagem):
-    frase = " "
-
-    for i in mensagem:
-        frase = frase + chr(ord(i) - 5)
-
-    return frase
+#     for i in palavra:
+#         mensagem = mensagem + chr(ord(i) + 5)
+#     return mensagem
 
 
-print("TESTE DE CRIPTOGRAFIA")
+# def descriptografar(mensagem):
+#     frase = " "
 
-palavra = criptografia(" vaESTE")
-print("Criptografado: " + palavra)
+#     for i in mensagem:
+#         frase = frase + chr(ord(i) - 5)
 
-palavra_descriptografada = descriptografar(palavra)
+#     return frase
 
-print("Descriptografado: " + palavra_descriptografada)
+
+# print("TESTE DE CRIPTOGRAFIA")
+
+# palavra = criptografia(" vaESTE")
+# print("Criptografado: " + palavra)
+
+# palavra_descriptografada = descriptografar(palavra)
+
+# print("Descriptografado: " + palavra_descriptografada)
+
+def vigenere(texto, chave, modo):
+    alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    texto = texto.upper()
+    chave = chave.upper()
+    resultado = ''
+
+    for i in range(len(texto)):
+        if texto[i] in alfabeto:
+            if modo == 'encriptar':
+                resultado += alfabeto[(ord(texto[i]) +
+                                       ord(chave[i % len(chave)]) - 2 * ord('A')) % 26]
+                print(ord(chave[i % len(chave)]))
+                print(resultado)
+            elif modo == 'desencriptar':
+                resultado += alfabeto[(ord(texto[i]) -
+                                       ord(chave[i % len(chave)]) + 26) % 26]
+        else:
+            resultado += texto[i]
+
+    print(resultado)
+    return resultado
+
+
+teste = vigenere('teste', 'key', 'encriptar')
+vigenere(teste, 'key', 'desencriptar')
