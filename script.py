c65afaf6c567c3a12bdbce1952628278fcbf407e
João Falcão <jgabfalcao@gmail.com>
2023-10-10 20:35:17 -0300

    feat: Inputs para receber palavra e chave

--- script.py
+++ script.py
@@ -1,28 +1,3 @@
-# def criptografia(palavra):
-#     mensagem = " "
-
-#     for i in palavra:
-#         mensagem = mensagem + chr(ord(i) + 5)
-#     return mensagem
-
-
-# def descriptografar(mensagem):
-#     frase = " "
-
-#     for i in mensagem:
-#         frase = frase + chr(ord(i) - 5)
-
-#     return frase
-
-
-# print("TESTE DE CRIPTOGRAFIA")
-
-# palavra = criptografia(" vaESTE")
-# print("Criptografado: " + palavra)
-
-# palavra_descriptografada = descriptografar(palavra)
-
-# print("Descriptografado: " + palavra_descriptografada)
 
 def vigenere(texto, chave, modo):
     alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -35,17 +10,19 @@ def vigenere(texto, chave, modo):
             if modo == 'encriptar':
                 resultado += alfabeto[(ord(texto[i]) +
                                        ord(chave[i % len(chave)]) - 2 * ord('A')) % 26]
-                print(ord(chave[i % len(chave)]))
-                print(resultado)
             elif modo == 'desencriptar':
                 resultado += alfabeto[(ord(texto[i]) -
                                        ord(chave[i % len(chave)]) + 26) % 26]
         else:
             resultado += texto[i]
 
-    print(resultado)
     return resultado
 
 
-teste = vigenere('teste', 'key', 'encriptar')
-vigenere(teste, 'key', 'desencriptar')
+texto_original = input('Digite a palavra para ser criptografada: ')
+chave = input('Digite a palavra chave: ')
+
+texto_criptografado = vigenere(texto_original, chave, 'encriptar')
+print('Texto criptografado:', texto_criptografado)
+texto_descriptografado = vigenere(texto_criptografado, chave, 'desencriptar')
+print('Texto descriptografado:', texto_descriptografado)
